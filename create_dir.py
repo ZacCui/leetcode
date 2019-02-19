beb97c5550d5e901539f2e6853ffdd1244f61da7
Zac Cui <zac.cui@hivetec.com.au>
2019-02-19 12:14:20 +1100

    add create dir script

---
+++ create_dir.py
@@ -0,0 +1,24 @@
+#!/usr/local/bin/python3
+
+import sys
+import os
+
+if len(sys.argv) != 2:
+	print('Usage : ./create_dir "<questionname>"')
+	exit()
+
+name_str = sys.argv[1]
+num = name_str.split(' ')[0]
+dirname = '_'.join(str(name_str).lower().split(' '))
+path = '{}/{}'.format(os.getcwd(), dirname)
+
+try:  
+    os.mkdir(path)
+except OSError:  
+    print ("Creation of the directory %s failed" % path)
+else:  
+    print ("Successfully created the directory %s " % path)
+
+with open('{}/{}.py'.format(path, num), 'w+') as f:
+	f.write('#!/usr/local/bin/python3')
+	print ("Successfully created the file %s " % '{}/{}.py'.format(path, num))
