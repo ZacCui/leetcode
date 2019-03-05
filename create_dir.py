c261ed4e16ce0db908c4b7bf1cc68a31676ddf2a
Ziyi Cui <ziyi.cui@student.unsw.edu.au>
2019-03-05 18:06:42 +1100

    improve create dir

--- create_dir.py
+++ create_dir.py
@@ -2,11 +2,14 @@
 
 import sys
 import os
+import datetime
 
 if len(sys.argv) != 2:
 	print('Usage : ./create_dir "<questionname>"')
 	exit()
 
+author = os.environ['FULL_NAME'] if os.environ.get('FULL_NAME') else 'Unknown'
+time = str(datetime.datetime.now()).split('.')[0]
 name_str = sys.argv[1].replace('.', '')
 num = name_str.split(' ')[0]
 dirname = '_'.join(str(name_str).lower().split(' '))
@@ -16,6 +19,7 @@ try:
     os.mkdir(path)
 except OSError:  
     print ("Creation of the directory %s failed" % path)
+    exit()
 else:  
     print ("Successfully created the directory %s " % path)
 
@@ -23,5 +27,9 @@ languages = ['py', 'java', 'cpp']
 
 for lan in languages:	
 	with open('{}/{}.{}'.format(path, num, lan), 'w+') as f:
-		if lan == 'py': f.write('#!/usr/local/bin/python3')
-		print ("Successfully created the file %s " % '{}/{}.py'.format(path, num))
+		if lan == 'py': 
+			f.write('#!/usr/local/bin/python3\n')
+			f.write("\'\'\'\n\tAuthor: {}\n\tCreated Date: {}\n\'\'\'".format(author, time))
+		else:
+			f.write("/*\n\tAuthor: {}\n\tCreated Date: {}\n*/".format(author, time))
+		print ("Successfully created the file %s " % '{}/{}.{}'.format(path, num, lan))
