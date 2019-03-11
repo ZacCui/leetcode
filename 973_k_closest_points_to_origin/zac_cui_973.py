357faa9b748f770e5a36993c9c49634581d31a93
Ziyi Cui <ziyi.cui@student.unsw.edu.au>
2019-03-11 12:44:16 +1100

    solution 973

--- 973_k_closest_points_to_origin/zac_cui_973.py
+++ 973_k_closest_points_to_origin/zac_cui_973.py
@@ -3,3 +3,8 @@
 	Author: Zac Cui
 	Created Date: 2019-03-11 11:49:21
 '''
+
+class Solution:
+    def kClosest(self, points: List[List[int]], K: int) -> List[List[int]]:
+        points.sort(key = lambda p: p[0]**2 + p[1]**2)
+        return points[:K]
