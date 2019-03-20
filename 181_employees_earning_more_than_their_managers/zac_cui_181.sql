0233f668a49bb114c03d03cd394daf10732cadd5
Zac Cui <zaccui@192-168-1-9.tpgi.com.au>
2019-03-20 19:38:14 +1100

    add 181

--- 181_employees_earning_more_than_their_managers/zac_cui_181.sql
+++ 181_employees_earning_more_than_their_managers/zac_cui_181.sql
@@ -1,4 +1,11 @@
-'''
-	Author: Zac Cui
-	Created Date: 2019-03-13 13:23:47
-'''
+--	Author: Zac Cui
+--	Created Date: 2019-03-13 13:23:47
+
+SELECT 
+    e.Name AS 'Employee'
+FROM
+    Employee e, Employee m
+WHERE
+    m.id = e.ManagerId
+AND
+    m.Salary < e.Salary
