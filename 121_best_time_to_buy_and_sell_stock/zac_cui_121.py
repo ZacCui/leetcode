e12786f25b53e99e6cdecd0ac343a9e606b0a110
Zac Cui <ziyi.cui@outlook.com>
2019-04-09 19:37:20 +1000

    update 121

--- 121_best_time_to_buy_and_sell_stock/zac_cui_121.py
+++ 121_best_time_to_buy_and_sell_stock/zac_cui_121.py
@@ -3,3 +3,13 @@
 	Author: Zac Cui
 	Created Date: 2019-04-09 19:36:20
 '''
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        if len(prices) < 2: return 0
+        mini = prices[0]
+        max_diff = 0
+        for i in range(1, len(prices)):
+            mini = min(prices[i], mini)
+            max_diff = max(prices[i] - mini, max_diff)
+        return max_diff
